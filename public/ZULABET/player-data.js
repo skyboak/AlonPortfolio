b463f0595214ea9e330ce094d1e1fde767b11362
Skyboak <Alonbarak13@gmail.com>
2026-06-02 16:13:01 +0300

    feat: add zulaBet page

---
+++ public/ZULABET/player-data.js
@@ -0,0 +1,45 @@
+/* eslint-env browser */
+window.playerDraftData = [
+  { name: 'Mohamed Salah', nation: 'Egypt', value: 24, shirtColor: '#7f1d1d' },
+  { name: 'Cole Palmer', nation: 'England', value: 20, shirtColor: '#1d4ed8' },
+  {
+    name: 'Bruno Fernandes',
+    nation: 'Portugal',
+    value: 18,
+    shirtColor: '#b91c1c',
+  },
+  {
+    name: 'Bryan Mbeumo',
+    nation: 'Cameroon',
+    value: 16,
+    shirtColor: '#f59e0b',
+  },
+  {
+    name: 'Dominic Solanke',
+    nation: 'England',
+    value: 14,
+    shirtColor: '#1d4ed8',
+  },
+  { name: 'Matheus Cunha', nation: 'Brazil', value: 15, shirtColor: '#16a34a' },
+  {
+    name: 'Chris Wood',
+    nation: 'New Zealand',
+    value: 12,
+    shirtColor: '#0f766e',
+  },
+  { name: 'Lamine Yamal', nation: 'Spain', value: 22, shirtColor: '#dc2626' },
+  { name: 'Kylian Mbappé', nation: 'France', value: 28, shirtColor: '#0f172a' },
+  {
+    name: 'Jude Bellingham',
+    nation: 'England',
+    value: 26,
+    shirtColor: '#1d4ed8',
+  },
+  {
+    name: 'Erling Haaland',
+    nation: 'Norway',
+    value: 25,
+    shirtColor: '#2563eb',
+  },
+  { name: 'Vinicius Jr.', nation: 'Brazil', value: 27, shirtColor: '#16a34a' },
+];
